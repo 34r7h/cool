3e9e15ef6a021cc8e7a2b5ff72933b40836e8ca2
candaras <mark.candaras@gmail.com>
2016-03-24 23:56:16 -0400

    fpv view, glasses and space are in. working out animations

---
+++ app/scripts/fpv/fpv-d.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+* @ngdoc directive
+* @name cool.directive:fpv
+* @description
+* # fpv
+*/
+angular.module('cool')
+    .directive('fpv', function ()
+    {
+        return {
+            templateUrl: 'scripts/fpv/fpv-d.html',
+            
+            restrict: 'EA',
+            link: function (scope, el, attrs)
+            {
+
+            },
+            controller: function ($scope)
+            {
+
+            }
+        };
+    });
