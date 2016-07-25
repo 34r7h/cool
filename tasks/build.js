012b43a628e074619999c2babebe54ef1270bd6a
births <irth03@gmail.com>
2016-07-24 21:04:02 -0400

    brittle tittle

--- tasks/build.js
+++ tasks/build.js
@@ -11,7 +11,8 @@ var gulp = require('gulp');
 var del = require('del');
 var gulpif = require('gulp-if');
 var minifyHtml = require('gulp-minify-html');
-var minifyCss = require('gulp-minify-css');
+// var minifyCss = require('gulp-minify-css');
+var cleanCss = require('gulp-clean-css');
 var useref = require('gulp-useref');
 var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
@@ -83,9 +84,9 @@ gulp.task('minFiles', function () {
     var assets = useref.assets();
     return gulp.src(config.mainFile)
         .pipe(assets)
-        .pipe(gulpif('*.js', ngAnnotate()))
-        .pipe(gulpif('*.js', uglify()))
-        .pipe(gulpif('*.css', minifyCss()))
+        // .pipe(gulpif('*.js', ngAnnotate()))
+        // .pipe(gulpif('*.js', uglify()))
+        // .pipe(gulpif('*.css', cleanCss()))
         .pipe(assets.restore())
         .pipe(useref())
         .pipe(gulp.dest(config.dist));
